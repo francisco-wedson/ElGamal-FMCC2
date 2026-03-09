5f827ebcbc75757e0d10944580e03f624fcc3cc9
nettoluis <luisnetto.2013@gmail.com>
2026-03-09 19:13:27 -0300

    add: Documentacao da funçao ExponenciacaoModular

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,9 @@
 package utils
 
+/*
+	Essa funcao calcula o resultado da exponenciacao modular, recebendo a base, o expoente e o modulo, respectivamente e retornando o valor do da exponenciacao modular
+	Baseado em: https://en.wikipedia.org/wiki/Modular_exponentiation#Memory-efficient_method
+*/
 func ExponenciacaoModular(base, expoente, modulo int) int {
 	if modulo == 1 {
 		return 0
@@ -12,3 +16,7 @@ func ExponenciacaoModular(base, expoente, modulo int) int {
 
 	return expoenteModular
 } 
+
+func InversoModular(valor, modulo int) int {
+	
+}
