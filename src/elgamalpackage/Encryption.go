53c1413f93f3cdf1f1217c410989f80d8e185b7f
G1gg1l3ws <guigo.gda@gmail.com>
2026-03-10 15:50:31 -0300

    Encryption stub

--- src/elgamalpackage/Encryption.go
+++ src/elgamalpackage/Encryption.go
@@ -1,15 +1,32 @@
 package elgamalpackage
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
-func geradorChavePublica(p int, elemGerador int) [3]int {
-	var privateKey int = rand.Int()
+func geradorChavePublica(generator big.Int, p big.Int, intermediate big.Int) (big.Int, big.Int) {
+	privateKey, _ := rand.Int(rand.Reader, &p)
 
-	var h int = ExponenciacaoModular(elemGerador, privateKey, p)
+	var publicKey = ExponeciacaoModular(intermediate, privateKey, p)
+	var h = ExponenciacaoModular(generator, privateKey, p)
 
-	var publicKey [3]int = [3]int{p, elemGerador, h}
-
-	return publicKey
+	return publicKey, h
 }
 
-func encriptacao(keyLength int, elemGerador int, Message int) {}
+func encriptacao(generator big.Int, p big.Int, intermediate big.Int, mensagem string) (big.Int, []big.Int) {
+	chavePublica, c1 := geradorChavePublica(generator, p, intermediate)
+
+	var c2 []big.Int
+	for _, chr := range mensagem {
+		//falta converter chr para int
+		c2 = append(c2, codificaDigito(big.Int(chr), chavePublica, p))
+	}
+
+	return c1, c2
+}
+
+// mul esta com parametros errados
+func codificaDigito(digito big.Int, chavePublica big.Int, p big.Int) big.Int {
+	return (big.Int.Mul(digito, chavePublica) % p)
+}
