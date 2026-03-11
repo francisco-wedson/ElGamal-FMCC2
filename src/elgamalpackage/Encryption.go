bdb53ebd182024f6f8170253d76ba7b90ba45027
G1gg1l3ws <guigo.gda@gmail.com>
2026-03-11 15:45:29 -0300

    O arquivo deve estar pronto, aguardando funções util receberem os parametros em big.Int()

--- src/elgamalpackage/Encryption.go
+++ src/elgamalpackage/Encryption.go
@@ -2,44 +2,51 @@ package elgamalpackage
 
 import (
 	"crypto/rand"
-	"math"
 	"math/big"
 
 	"../utils"
 )
 
-func geradorChavePublica(generator big.Int, p big.Int, intermediate big.Int) (*big.Int, *big.Int) {
+func GeradorChavePublica(generator big.Int, p big.Int, intermediate big.Int) (*big.Int, *big.Int) {
+
 	privateKey, _ := rand.Int(rand.Reader, &p)
 
-	var publicKeyInt = utils.ExponenciacaoModular(int(intermediate.Int64()), int(privateKey.Int64()), int(p.Int64()))
-	var cipherText1Int = utils.ExponenciacaoModular(int(generator.Int64()), int(privateKey.Int64()), int(p.Int64()))
-	var publicKey = big.NewInt(int64(publicKeyInt))
-	var cipherText1 = big.NewInt(int64(cipherText1Int))
+	var publicKey = utils.ExponenciacaoModular(intermediate, privateKey, p)
+	var cipherText1 = utils.ExponenciacaoModular(generator, privateKey, p)
 
 	return publicKey, cipherText1
 }
 
-func encriptacao(generator *big.Int, keySize int, intermediate *big.Int, mensagem string) (*big.Int, []*big.Int) {
-	prime, _ := rand.Int(rand.Reader, big.NewInt(Pow(2, keySize)))
+func Encriptacao(generator *big.Int, keySize int, prime *big.Int, intermediate *big.Int, mensagem string) (*big.Int, []*big.Int) {
 
-	K, c1 := geradorChavePublica(*generator, *prime, *intermediate)
+	K, c1 := GeradorChavePublica(*generator, *prime, *intermediate)
 
 	var c2 []*big.Int
-	for _, chr := range mensagem {
+
+	mensagemBytes := StringToInt(mensagem)
+
+	for _, letra := range mensagemBytes {
 		//falta converter chr para int
-		c2 = append(c2, codificaDigito(big.Int(chr), *K, *prime))
+
+		c2 = append(c2, CodificaDigito(*big.NewInt(letra), *K, *prime))
 	}
 
 	return c1, c2
 }
 
-func codificaDigito(digito big.Int, chavePublica big.Int, p big.Int) *big.Int {
+func CodificaDigito(digito big.Int, chavePublica big.Int, p big.Int) *big.Int {
 	var mult big.Int
 	mult.Mul(&digito, &chavePublica)
 
 	return mult.Rem(&mult, &p)
 }
 
-func Pow(x int, y int) int64 {
-	return int64(math.Pow(float64(x), float64(y)))
+func StringToInt(String string) []int64 {
+	//1. Converter string para sequencia de bytes
+	var StringBytes []int64
+	for _, char := range String {
+		StringBytes = append(StringBytes, int64(char))
+	}
+
+	return StringBytes
 }
