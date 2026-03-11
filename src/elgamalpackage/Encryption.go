68603d7eb9899b71d0187829c694d3c8592a8391
G1gg1l3ws <guigo.gda@gmail.com>
2026-03-10 22:35:37 -0300

    codificaDigito (não testado, por enquanto)

--- src/elgamalpackage/Encryption.go
+++ src/elgamalpackage/Encryption.go
@@ -11,29 +11,33 @@ import (
 func geradorChavePublica(generator big.Int, p big.Int, intermediate big.Int) (*big.Int, *big.Int) {
 	privateKey, _ := rand.Int(rand.Reader, &p)
 
-	var publicKey = big.NewInt(int64(utils.ExponenciacaoModular(int(intermediate.Int64()), int(privateKey.Int64()), int(p.Int64()))))
-	var h = big.NewInt(int64(utils.ExponenciacaoModular(int(generator.Int64()), int(privateKey.Int64()), int(p.Int64()))))
+	var publicKeyInt = utils.ExponenciacaoModular(int(intermediate.Int64()), int(privateKey.Int64()), int(p.Int64()))
+	var cipherText1Int = utils.ExponenciacaoModular(int(generator.Int64()), int(privateKey.Int64()), int(p.Int64()))
+	var publicKey = big.NewInt(int64(publicKeyInt))
+	var cipherText1 = big.NewInt(int64(cipherText1Int))
 
-	return publicKey, h
+	return publicKey, cipherText1
 }
 
 func encriptacao(generator *big.Int, keySize int, intermediate *big.Int, mensagem string) (*big.Int, []*big.Int) {
 	prime, _ := rand.Int(rand.Reader, big.NewInt(Pow(2, keySize)))
 
-	chavePublica, c1 := geradorChavePublica(*generator, *prime, *intermediate)
+	K, c1 := geradorChavePublica(*generator, *prime, *intermediate)
 
 	var c2 []*big.Int
 	for _, chr := range mensagem {
 		//falta converter chr para int
-		c2 = append(c2, codificaDigito(big.Int(chr), *chavePublica, *prime))
+		c2 = append(c2, codificaDigito(big.Int(chr), *K, *prime))
 	}
 
 	return c1, c2
 }
 
-// mul esta com parametros errados
 func codificaDigito(digito big.Int, chavePublica big.Int, p big.Int) *big.Int {
-	return (big.Int.Mul(digito, chavePublica) % &p)
+	var mult big.Int
+	mult.Mul(&digito, &chavePublica)
+
+	return mult.Rem(&mult, &p)
 }
 
 func Pow(x int, y int) int64 {
